4e9ad25910f76029bd2463ff6944bc3d757a7ce6
Caio Braga <caiotabraga@hotmail.com>
2025-09-22 14:07:48 -0300

    Setting application stateless

--- src/main/java/br/com/forum_hub/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/br/com/forum_hub/infra/security/ConfiguracoesSeguranca.java
@@ -2,9 +2,12 @@ package br.com.forum_hub.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +16,11 @@ public class ConfiguracoesSeguranca {
   @Bean
   public PasswordEncoder encriptador() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+    return http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .csrf(csrf -> csrf.disable()).build();
   }
 }
